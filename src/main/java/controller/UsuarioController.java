ee7b4077892a1470958cdc283602368fd501bae1
Isabella Chaves <isaachaves9@gmail.com>
2026-09-04 17:08:49 -0300

    feat: novas funcionalidades do CRUD
    Added methods for deleting and updating users by ID.

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -13,9 +13,12 @@ public class UsuarioController {
 //atributo da array list que irá guardar os usuarios
     private List<Usuario> listaUsuario = new ArrayList<>();
 
+    private Long proximoId = 1L;
+
 //metodos para salvar usuarios na array list
     @PostMapping
     public Usuario salvarUsuario(@RequestBody Usuario novoUsuario){
+        novoUsuario.setIdUsuario(proximoId++);
         listaUsuario.add(novoUsuario);
         return novoUsuario;
     }
@@ -25,6 +28,7 @@ public class UsuarioController {
     return listaUsuario;
     }
 
+//metodo para buscar usuario por id
     @GetMapping("/{id}")
     public Usuario buscaPorId(@PathVariable Long id){
         for (Usuario pessoa : listaUsuario){
@@ -34,4 +38,30 @@ public class UsuarioController {
         }
         return null;
     }
+
+//
+    @DeleteMapping("/{id}")
+    public String deletarUsuario(@PathVariable Long id) {
+        boolean removido = listaUsuario.removeIf(pessoa -> pessoa.getIdUsuario().equals(id));
+
+        if (removido) {
+            IO.println("Usuário removido com sucesso!");
+        }
+        return "Usuário não encontrado";
+    }
+
+    @PutMapping("/{id}")
+    public Usuario atualizarUsuario (@PathVariable Long id, @RequestBody Usuario dadosNovos){
+        for (Usuario pessoa : listaUsuario){
+            if (pessoa.getIdUsuario().equals(id)){
+                pessoa.setNomeCompleto(dadosNovos.getNomeCompleto());
+                pessoa.setDtNascimento(dadosNovos.getDtNascimento());
+                pessoa.setEmail(dadosNovos.getEmail());
+                pessoa.setBiografia(dadosNovos.getBiografia());
+            }
+            return pessoa;
+        }
+        return null;
+    }
+
 }
